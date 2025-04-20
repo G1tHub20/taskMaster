99f9a11e3fc81e6df7e6bf1a42aa6d0f23066d3d
G1tHub20 <a7m2cy@gmail.com>
2025-04-20 12:03:40 +0900

    完了済みタスクの移動

--- js/main.js
+++ js/main.js
@@ -5,11 +5,21 @@ const app = Vue.createApp({
     newItem: '',
     tasks: []
   }),
+  // 算出プロパティ（処理した結果を返す）
+  computed: {
+    incompleteTasks() {
+      return this.tasks.filter(task => !task.isDone)
+    },
+    completeTasks() {
+      return this.tasks.filter(task => task.isDone)
+    },
+  },
   methods: {
     addItem: function(event) {
       if (this.newItem === '') return // 空文字だったら処理しない
       // taskオブジェクトの作成
       let task = {
+        id: Date.now(), // 一意なID
         item: this.newItem,
         isDone: false,
         deadLine: ''
